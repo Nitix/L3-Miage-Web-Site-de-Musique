a50cfcb26f28fa35d00750c42cd0bbb02f8f53be
Mouna24 <mouna.ak24@yahoo.com>
2014-11-27 20:11:37 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+/**
+ * Created by mouna on 27/11/14.
+ */
+
+function DivInscription(){
+    var defaut = document.getElementById('divDefaut');
+    var conx = document.getElementById('divConx');
+    var insc = document.getElementById('divInsc');
+
+    defaut.style.display="none";
+    conx.style.display="none";
+    insc.style.display="block";
+}
+
+function DivConnexion(){
+    var defaut = document.getElementById('divDefaut');
+    var conx = document.getElementById('divConx');
+    var insc = document.getElementById('divInsc');
+
+    defaut.style.display="none";
+    insc.style.display ="none";
+    conx.style.display ="block";
+}
