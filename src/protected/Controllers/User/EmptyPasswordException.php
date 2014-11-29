496b0bad37d49575d754a5d183ebdb022ef6bfd5
Nitix <guillaume.pierson.gp@gmail.com>
2014-11-29 01:31:24 +0100

    Add general and interface user controller Does not work, need class User in Model

---
+++ src/protected/Controllers/User/EmptyPasswordException.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Controllers\User;
+
+/**
+ * Exception thrown when the password is empty or null
+ * @package Controllers\User
+ */
+class EmptyPasswordException extends \Exception
+{
+
+} 
---
+++ src/protected/Controllers/User/WeakPasswordException.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Controllers\User;
+
+/**
+ * Exception thrown when the password is too much weak
+ * @package Controllers\User
+ */
+class WeakPasswordException extends \Exception
+{
+
+} 
