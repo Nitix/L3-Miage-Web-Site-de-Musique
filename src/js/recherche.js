01e4be42114658aaf748a2638fed5c72af1be158
Nitix <guillaume.pierson.gp@gmail.com>
2014-12-16 01:41:54 +0100

    Corrected some files and use of Models for autocomplete

--- src/js/recherche.js
+++ src/js/recherche.js
@@ -1,15 +1,29 @@
-$('#recherche').autocomplete({
-    source : 'index.php?c=base&a=autocomplete'
-});
-
-/*
-$.ajax({
-    url: 'index.php?c=base&a=recherche',
-    success: function(data){
-        console.log(data);
+$.widget( "custom.catcomplete", $.ui.autocomplete, {
+    _create: function() {
+        this._super();
+        this.widget().menu( "option", "items", "> :not(.ui-autocomplete-category)" );
+    },
+    _renderMenu: function( ul, items ) {
+        var that = this,
+            currentCategory = "";
+        $.each( items, function( index, item ) {
+            var li;
+            if ( item.category != currentCategory ) {
+                ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
+                currentCategory = item.category;
+            }
+            li = that._renderItemData( ul, item );
+            if ( item.category ) {
+                li.attr( "aria-label", item.category + " : " + item.label );
+            }
+        });
     }
 });
-*/
+
+$( "#recherche" ).catcomplete({
+    source: 'index.php?c=base&a=autocomplete'
+});
+
 
 //ajout de l'evenement pour lancer la recherche avec la touche Entree
  $("#recherche").keydown(function(event){
